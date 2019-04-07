3bfb9f1fb314775be867a26f848c89e99545b96c
Daniel Sørenes <Danielsorenes@hotmail.no>
2019-04-07 14:54:04 +0200

    Simple support added for writing to file

--- src/main/java/input/InputController.java
+++ src/main/java/input/InputController.java
@@ -4,8 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import person.Person;
+import savetofile.SaveToJOBJ;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class InputController implements Initializable {
@@ -19,9 +23,17 @@ public class InputController implements Initializable {
     @FXML
     Button submit;
 
+    @FXML
+    Button save;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        submit.setOnAction(e -> System.out.println(firstname.getText() + ", " + lastname.getText()));
+        List<Person> list = new ArrayList<>();
+        SaveToJOBJ stj = new SaveToJOBJ();
+        submit.setOnAction(e -> list.add(new Person(firstname.getText(), lastname.getText())));
+
+
+       save.setOnAction(e -> stj.SaveToFile("C:\\Users\\Daniel\\OneDrive - OsloMet\\Programutvikling\\Semesteroppgave\\person.txt", list));
     }
 }
---
+++ src/main/java/person/Person.java
@@ -0,0 +1,18 @@
+package person;
+
+public class Person {
+
+    public String firstname;
+    public String lastname;
+
+    public Person (String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString () {
+        return this.firstname + " " + this.lastname;
+    }
+
+}
