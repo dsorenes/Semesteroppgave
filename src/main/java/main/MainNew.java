e4fcd8fb18ab077d94743b1d206d0be561414e27
Haumar <Hau.mar180@gmail.com>
2019-04-26 19:02:17 +0200

    Midlertidig  Måtte sette denne opp slik at JavaFX bli initialisert i JAR applikasjonen.

---
+++ src/main/java/main/MainNew.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+
+ Midlertidig
+ Måtte sette denne opp slik at JavaFX bli initialisert i JAR applikasjonen.
+
+*/
+
+public class MainNew {
+
+        public static void main(String[] args) {
+
+            Main.main(args);
+
+        }
+
+
+
+}
