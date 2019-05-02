248f67b466218dfdda3781d5f93746fa2c1033b5
Haumar <Hau.mar180@gmail.com>
2019-05-02 04:15:48 +0200

    Reformatert, lagd set methoder for validering.

--- src/main/java/model/data/substitute/work/Work.java
+++ src/main/java/model/data/substitute/work/Work.java
@@ -19,6 +19,8 @@ public class Work {
     private Substitute substitute;
     private int substituteID;
 
+    public Work() {}
+
     public Work(String companyName, String position, Sector sector, Industry industry, LocalDate employedFrom, LocalDate employedTo) {
         this.companyName = companyName;
         this.position = position;
@@ -43,44 +45,49 @@ public class Work {
         return substituteID;
     }
 
-    @Override
-    public String toString() {
-        return this.workID + ";" + substituteID + ";" + this.companyName + ";" + this.position + ";" + this.sector + ";" + this.industry + ";" + this.employedFrom + ";" + this.employedTo;
-    }
-
     public void setSubstituteID(int ID) {
         this.substituteID = ID;
     }
 
+    public int getID () { return this.workID; }
     public void setID (int ID) {
         this.workID = ID;
-    }
-
-    public int getID () {
-        return this.workID;
     }
 
     public String getCompanyName() {
         return companyName;
     }
+    public void setCompanyName(String companyName) { this.companyName = companyName; }
 
     public String getPosition() {
         return position;
     }
+    public void setPosition(String position) { this.position = position; }
 
     public Sector getSector() {
         return sector;
     }
+    public void setSector(Sector sector) { this.sector = sector; }
 
     public Industry getIndustry() {
         return industry;
     }
+    public void setIndustry(Industry industry) { this.industry = industry; }
 
     public LocalDate getEmployedFrom() {
         return employedFrom;
     }
+    public void setEmployedFrom(LocalDate employedFrom) { this.employedFrom = employedFrom; }
 
     public LocalDate getEmployedTo() {
         return employedTo;
     }
+    public void setEmployedTo(LocalDate employedTo) { this.employedTo = employedTo; }
+
+    @Override
+    public String toString() {
+        return this.workID + ";" + substituteID + ";" + this.companyName + ";" + this.position + ";" + this.sector + ";" + this.industry + ";" + this.employedFrom + ";" + this.employedTo;
+    }
+
+
 }
